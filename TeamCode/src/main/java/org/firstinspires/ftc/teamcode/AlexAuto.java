3c5c9ae7ccd6712d98bf438d6b42ec2453131b65
adevine22 <adevine22@us.edu>
2021-09-10 16:43:41 -0400

    Alex Auto Initial Commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AlexAuto.java
@@ -0,0 +1,66 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.Gyroscope;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+@Autonomous
+public class AlexAuto extends LinearOpMode {
+
+    //private Gyroscope imu;
+    //private DigitalChannel digitalTouch;
+    //private DistanceSensor sensorColorRange;
+    //private Servo servoTest;
+
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor frontLeft = null;
+    private DcMotor frontRight = null;
+    private DcMotor backLeft = null;
+    private DcMotor backRight = null;
+
+    @Override
+    public void runOpMode() {
+
+        // imu = hardwareMap.get(Gyroscope.class, "imu");
+
+        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+        backRight = hardwareMap.get(DcMotor.class, "backRight");
+
+        //  digitalTouch = hardwareMap.get(DigitalChannel.class, "digitalTouch");
+        // sensorColorRange = hardwareMap.get(DistanceSensor.class, "sensorColorRange");
+        // servoTest = hardwareMap.get(Servo.class, "servoTest");
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+// Wait for the game to start (driver presses PLAY)
+
+        waitForStart();
+        runtime.reset();
+
+// run until the end of the match (driver presses STOP)
+
+        while (opModeIsActive()) {
+            double leftPower = 0.5;
+            double rightPower = 0.5;
+
+
+            frontLeft.setPower(leftPower);
+            frontRight.setPower(rightPower);
+            backRight.setPower(rightPower);
+            backLeft.setPower(leftPower);
+
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
+            telemetry.update();
+        }
+    }
+}
