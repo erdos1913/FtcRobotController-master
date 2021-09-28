62a48366665beb33c844bdfcf14f7ccd705f0a39
adevine22 <adevine22@us.edu>
2021-09-28 16:25:55 -0400

    Alex Cleanup

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AlexAutoTime.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AlexAutoTime.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
@@ -11,6 +12,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
 @Autonomous
+@Disabled
 public class AlexAutoTime extends LinearOpMode {
 
     //private Gyroscope imu;
